43c97af9e8b58bb2616e76552d5f64a52e56e942
Tamas Szabo <szabtam@gmail.com>
2012-08-04 23:21:49 +0800

    Refactor extracts common code in Log abstract model and LoginLogger class.

--- userlog/backend.py
+++ userlog/backend.py
@@ -1,28 +1,14 @@
 from userlog import models as m
 from userlog.middleware import get_request
 
-def get_request_header(name):
-    request = get_request()
-    if request and name in request.META:
-        return request.META[name]
-
 class AuthFailedLoggerBackend(object):
+
+    def __init__(self):
+        self.login_logger = m.LoginLogger()
 
     def authenticate(self, **credentials):
         username = credentials.get('username')
-        ip_address = self.extract_ip_address()
-        user_agent = get_request_header('USER_AGENT')
-        m.FailedLoginLog.objects.create(
-            username=username, 
-            ip_address=ip_address,
-            user_agent=user_agent
-        )
+        self.login_logger.log_failed_login(username, get_request())
 
         return None
 
-    def extract_ip_address(self):
-        ip = get_request_header('REMOTE_ADDR')
-        forwarded_for = get_request_header('X_FORWARDED_FOR')
-        if forwarded_for is not None:
-            ip = forwarded_for.split(',')[0].strip()
-        return ip
--- userlog/models.py
+++ userlog/models.py
@@ -1,40 +1,58 @@
 from django.db import models
 from django.contrib.auth.signals import user_logged_in
 
-# TODO
-#class Log(models.Model):
+class Log(models.Model):
+    class Meta:
+        abstract = True
+        ordering = ['-timestamp']
 
-
-class FailedLoginLog(models.Model):
     username = models.CharField(max_length=255, null=True, blank=True)
     ip_address = models.CharField(max_length=40, null=True, blank=True)
     user_agent = models.CharField(max_length=255, null=True, blank=True)
     timestamp = models.DateTimeField(auto_now_add=True)
 
-class LoginLog(models.Model):
-    username = models.CharField(max_length=255, null=True, blank=True)
-    ip_address = models.CharField(max_length=40, null=True, blank=True)
-    user_agent = models.CharField(max_length=255, null=True, blank=True)
-    timestamp = models.DateTimeField(auto_now_add=True)
+class FailedLoginLog(Log):
+    pass
 
-def log_login(sender, user, request, **kwargs):
-    ip_address = None
-    user_agent = None
-    if request:
-        ip_address = extract_ip_address(request)
-        user_agent = request.META.get('USER_AGENT')
-    LoginLog.objects.create(
-        username = user.username,
-        ip_address = ip_address,
-        user_agent = user_agent
-    )
+class LoginLog(Log):
+    pass
+
+class LoginLogger(object):
+
+    def log_failed_login(self, username, request):
+        fields = self.extract_log_info(username, request)
+        log = FailedLoginLog.objects.create(**fields)
+
+    def log_login(self, username, request):
+        fields = self.extract_log_info(username, request)
+        log = LoginLog.objects.create(**fields)
+
+    def extract_log_info(self, username, request):
+        ip_address = None
+        user_agent = None
+        if request:
+            ip_address = self.extract_ip_address(request)
+            user_agent = request.META.get('USER_AGENT')
+        return {
+            'username': username,
+            'ip_address': ip_address,
+            'user_agent': user_agent
+        }
+
+    def extract_ip_address(self, request):
+        ip = request.META.get('REMOTE_ADDR')
+        forwarded_for = request.META.get('X_FORWARDED_FOR')
+        if forwarded_for is not None:
+            ip = forwarded_for.split(',')[0].strip()
+        return ip
+
+  
+
+login_logger = LoginLogger()
+def login_callback(sender, user, request, **kwargs):
+    login_logger.log_login(user.username, request)
     
-def extract_ip_address(request):
-    ip = request.META.get('REMOTE_ADDR')
-    forwarded_for = request.META.get('X_FORWARDED_FOR')
-    if forwarded_for is not None:
-        ip = forwarded_for.split(',')[0].strip()
-    return ip
+# User logged in Django signal
+user_logged_in.connect(login_callback)
 
-user_logged_in.connect(log_login)
 
