7000e9df55a144e5b8c7f639a3a368c467545c57
Rodney Lorrimar <rlorrimar@ccg.murdoch.edu.au>
2014-08-07 13:09:41 +0800

    Let the auth backend handle custom User models
    The auth backend falls back to the "username" field because the Django
    admin form uses that.

--- userlog/backend.py
+++ userlog/backend.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from userlog import models as m
 from userlog.middleware import get_request
 
@@ -9,8 +10,9 @@ class AuthFailedLoggerBackend(object):
         self.login_logger = m.LoginLogger()
 
     def authenticate(self, **credentials):
-        username = credentials.get('username')
+        UserModel = get_user_model()
+        username = credentials.get(UserModel.USERNAME_FIELD, None)
+        username = username or credentials.get('username')
         self.login_logger.log_failed_login(username, get_request())
 
         return None
-
