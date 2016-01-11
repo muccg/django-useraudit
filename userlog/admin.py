87cae9e5c244f99a0595adc6cc10267a6b2d426c
Rodney Lorrimar <rlorrimar@ccg.murdoch.edu.au>
2016-01-11 08:18:23 +0000

    Django 1.9 compatibility

--- userlog/admin.py
+++ userlog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-
 from userlog import models as m
+
 
 class LogAdmin(admin.ModelAdmin):
     model = m.Log
@@ -8,13 +8,8 @@ class LogAdmin(admin.ModelAdmin):
     search_fields = ['username']
     list_filter = ['timestamp']
     list_display = ('username', 'ip_address', 'forwarded_by', 'user_agent', 'timestamp')
+    list_display_links = None
 
-
-    # this disable the edit links. No supported way of doing it.
-    def __init__(self, *args, **kwargs):
-        admin.ModelAdmin.__init__(self, *args, **kwargs)
-        self.list_display_links = (None, )
 
 admin.site.register(m.LoginLog, LogAdmin)
 admin.site.register(m.FailedLoginLog, LogAdmin)
-
