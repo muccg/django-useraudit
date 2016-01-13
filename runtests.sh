27b0d654809b2fb4804eb0c3348ce301be0baf41
Rodney Lorrimar <rlorrimar@ccg.murdoch.edu.au>
2016-01-13 19:33:31 +0000

    Add tox setup

--- runtests.sh
+++ runtests.sh
@@ -4,4 +4,10 @@ cd `dirname $0`
 
 export PYTHONPATH=.
 django-admin.py test --settings=userlog.test_settings userlog
+EXIT1=$?
 django-admin.py test --settings=userlog_testapp.settings userlog_testapp
+EXIT2=$?
+
+if [ $EXIT1 -ne 0 -o $EXIT2 -ne 0 ]; then
+   exit 1
+fi
