82b74550f319312513ee6a85717729de5bc5cd0e
Rodney Lorrimar <rlorrimar@ccg.murdoch.edu.au>
2016-01-14 12:33:12 +0000

    rework: Update tests

--- userlog_testapp/settings.py
+++ userlog_testapp/settings.py
@@ -3,16 +3,18 @@ INSTALLED_APPS = [
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
+    "django.contrib.sites",
     "userlog",
     "userlog_testapp"
 ]
 AUTH_USER_MODEL = "userlog_testapp.MyUser"
 AUTH_USER_MODEL_PASSWORD_CHANGE_DATE_ATTR = "password_change_date"
-# PASSWORD_EXPIRY_DAYS = 10
+PASSWORD_EXPIRY_DAYS = 10
 # PASSWORD_EXPIRY_WARNING_DAYS = 7
-# ACCOUNT_EXPIRY_DAYS = 5
+ACCOUNT_EXPIRY_DAYS = 5
 AUTHENTICATION_BACKENDS = (
-    'userlog.password_expiry.UserExpiryBackend',
+    'userlog.password_expiry.AccountExpiryBackend',
+    'userlog.password_expiry.PasswordExpiryBackend',
     'django.contrib.auth.backends.ModelBackend',
     'userlog.backend.AuthFailedLoggerBackend',
 )
@@ -24,3 +26,5 @@ DATABASES = {
 }
 SECRET_KEY = "."
 TEMPLATES = []
+MIDDLEWARE_CLASSES = []
+SITE_ID = 1
--- userlog_testapp/tests.py
+++ userlog_testapp/tests.py
@@ -1,9 +1,11 @@
 from datetime import timedelta
-
-from django.test import TestCase, override_settings
 from django.contrib.auth import authenticate
-from django.utils import timezone
+from django.core import mail
+from django.core import management
 from django.dispatch import receiver
+from django.test import TestCase, override_settings
+from django.utils import timezone
+import re
 
 from userlog_testapp.models import MyUser
 import userlog.password_expiry
@@ -14,10 +16,13 @@ class ExpiryTestCase(TestCase):
     password = "testuser"
 
     def setUp(self):
-        self.user = MyUser.objects.create(username=self.username)
+        self.user = MyUser.objects.create(
+            username=self.username,
+            last_login=timezone.now(),
+            password_change_date=timezone.now(),
+            email="testuser@localhost",
+        )
         self.user.set_password(self.password)
-        self.user.last_login = timezone.now()
-        self.user.password_change_date = timezone.now()
         self.user.save()
 
         type(self).password_expired_signal = None
@@ -85,7 +90,7 @@ class ExpiryTestCase(TestCase):
         u = self.authenticate()
         self.assertIsNone(u)
         self.assertTrue(self.user2.is_active)
-        
+
     @override_settings(PASSWORD_EXPIRY_DAYS=-5)
     def test_password_expiry_disabled(self):
         self.setuser(password_change_date=timezone.now() - timedelta(days=10000))
@@ -107,6 +112,17 @@ class ExpiryTestCase(TestCase):
         self.assertIsNotNone(self.password_expired_signal)
         self.assertEquals(self.password_expired_signal["sender"], type(self.user))
         self.assertEquals(self.password_expired_signal["user"], self.user)
+
+
+    ###########################################################################
+    # account expiry notification test cases
+
+    @override_settings(ACCOUNT_EXPIRY_DAYS=5)
+    def test_command_notification(self):
+        self.setuser(last_login=timezone.now() - timedelta(days=6))
+        management.call_command("disable_inactive_users", verbosity=0, email=True)
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertTrue(re.search(r"expired", mail.outbox[0].subject, re.I))
 
 
 @receiver(userlog.password_expiry.password_has_expired)
