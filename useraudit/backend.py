39cfdb2d3d2afdff5d4649cb5df63c0212c34e77
Tamas Szabo <tszabo@ccg.murdoch.edu.au>
2016-06-22 10:50:24 +0300

    Reset failed login counter when user is re-actived (fixes #4)

--- useraudit/backend.py
+++ useraudit/backend.py
@@ -3,6 +3,8 @@ import abc
 from django.contrib.auth import get_user_model
 from django.conf import settings
 from django.core.exceptions import PermissionDenied
+from django.dispatch import receiver
+from django.db.models.signals import pre_save
 from .models import LoginLogger, LoginAttempt
 from .models import LoginAttemptLogger
 from .middleware import get_request
@@ -10,13 +12,25 @@ from .middleware import get_request
 logger = logging.getLogger("django.security")
 
 
-class AuthFailedLoggerBackend(object):
+@receiver(pre_save, sender=settings.AUTH_USER_MODEL)
+def user_pre_save(sender, instance=None, raw=False, **kwargs):
+    user = instance
+    is_new_user = user.pk is None
+    if is_new_user or raw:
+        return
 
-    supports_inactive_user = False
+    # User has been re-activated. Ensure the failed login counter is set to 0 so
+    # that the user isn't inactivated on next login by the AuthFailedLoggerBackend
+    current_user = sender.objects.get(pk=user.pk)
+    if not current_user.is_active and user.is_active:
+        LoginAttemptLogger().reset(user.username)
+
+
+class AuthFailedLoggerBackend(object):
 
     def __init__(self):
         self.login_logger = LoginLogger()
-        self.login_limit = getattr(settings, 'LOGIN_FAILURE_LIMIT', None)
+        self.login_failure_limit = getattr(settings, 'LOGIN_FAILURE_LIMIT', None) or 0
         self.login_attempt_logger = LoginAttemptLogger()
 
     def authenticate(self, **credentials):
@@ -24,32 +38,33 @@ class AuthFailedLoggerBackend(object):
         self.username = credentials.get(get_user_model().USERNAME_FIELD)
         self.login_logger.log_failed_login(self.username, get_request())
         self.login_attempt_logger.increment(self.username)
-        self.attemps_status()
+        self.block_user_if_needed()
 
         return None
 
-    def attemps_status(self):
-        if self.is_login_limit():
-            logger.info("Login failure limit is enabled")
-            if self.is_attempts_exceeded() and self._deactivate_user():
-                self.notification()
-                raise PermissionDenied("Username '%s' has been blocked" % self.username)
+    def block_user_if_needed(self):
+        if not self.is_login_failure_limit_enabled():
+            return
+        logger.debug("Login failure limit is enabled")
+        if self.is_attempts_exceeded():
+            self._deactivate_user()
+            self.notification()
+            logger.info("Login Prevented for user '%s'! Maximum failed logins %d reached!",
+                    self.username, self.login_failure_limit)
+            raise PermissionDenied("Username '%s' has been blocked" % self.username)
 
+    def is_login_failure_limit_enabled(self):
+        return self.login_failure_limit > 0
 
-    def is_login_limit(self):
-        if self.login_limit and self.login_limit > 0:
-            return True
-        return False
-    
     def is_attempts_exceeded(self,):
         count = self._get_count()
-        if count and count >= self.login_limit:
+        if count and count >= self.login_failure_limit:
             return True
         return False
-    
+
     def _get_count(self):
         try:
-            obj = LoginAttempt.objects.get(username = self.username)
+            obj = LoginAttempt.objects.get(username=self.username)
             return obj.count
         except LoginAttempt.DoesNotExist:
             return None
@@ -70,7 +85,7 @@ class AuthFailedLoggerBackend(object):
             logger.warning("Username '%s' has been blocked" % self.username)
             return True
         return False
-  
+
     @abc.abstractmethod
     def notification(self):
         """Place for notification to user that the account has been blocked (Email, SMS etc.)"""
--- useraudit_testapp/tests.py
+++ useraudit_testapp/tests.py
@@ -26,7 +26,7 @@ USER_MODEL = settings.AUTH_USER_MODEL
 # Also see doc for USER_MODEL above
 @receiver(setting_changed)
 def register_pre_save_on_AUTH_USER_MODER_change(sender, setting, value, enter, **kwargs):
-    if setting == 'AUTH_USER_MODEL' and value != USER_MODEL:
+    if setting == "AUTH_USER_MODEL" and value != USER_MODEL:
         if enter:
             pre_save.connect(useraudit.password_expiry.user_pre_save, sender=value)
         else:
@@ -98,7 +98,7 @@ class ExpiryTestCase(TestCase):
         self.assertTrue(u.is_active)
 
     @override_settings(ACCOUNT_EXPIRY_DAYS=5)
-    def test_expired_works_if_reactivated(self):
+    def test_authentication_works_if_reactivated(self):
         self.setuser(last_login=timezone.now() - timedelta(days=6))
         u = self.authenticate()
         # User is inactive now
@@ -117,7 +117,7 @@ class ExpiryTestCase(TestCase):
     def test_changing_of_password_updates_password_change_date_field(self):
         self.setuser(password_change_date=timezone.now() - timedelta(days=1))
         before_the_change = timezone.now()
-        self.user.set_password('the new password')
+        self.user.set_password("the new password")
         self.user.save()
         self.user.refresh_from_db()
         self.assertGreater(self.user.password_change_date, before_the_change)
@@ -127,7 +127,7 @@ class ExpiryTestCase(TestCase):
     def test_saving_same_password_does_not_update_password_change_date_field(self):
         one_day_ago = password_change_date=timezone.now() - timedelta(days=1)
         self.setuser(password_change_date=one_day_ago)
-        self.user.set_password('testuser') # same password
+        self.user.set_password("testuser") # same password
         self.user.save()
         self.user.refresh_from_db()
         self.assertEquals(one_day_ago, self.user.password_change_date, "Password change date shouldn't change")
@@ -232,7 +232,7 @@ class ProfileExpiryTestCase(TestCase):
         self.user.myprofile.password_change_date = timezone.now() - timedelta(days=1)
         self.user.myprofile.save()
         before_the_change = timezone.now()
-        self.user.set_password('the new password')
+        self.user.set_password("the new password")
         self.user.save()
         self.user.refresh_from_db()
         self.assertGreater(self.user.myprofile.password_change_date, before_the_change)
@@ -269,3 +269,73 @@ class ProfileExpiryTestCase(TestCase):
         u = self.authenticate()
         self.assertIsNone(u)
         self.assertTrue(self.user2.is_active)
+
+
+@override_settings(LOGIN_FAILURE_LIMIT=2)
+class FailedLoginAttemtpsTestCase(TestCase):
+    username = "testuser"
+    password = "testuser"
+
+    def setUp(self):
+        self.user = User.objects.create(
+            username=self.username,
+            email="testuser@localhost",
+        )
+        self.user.set_password(self.password)
+        self.user.save()
+
+    def tearDown(self):
+        self.user.delete()
+
+    @property
+    def user2(self):
+        self.user.refresh_from_db()
+        return self.user
+
+
+    def test_authenticate_works(self):
+        u = authenticate(username=self.username, password=self.password)
+        self.assertIsNotNone(u)
+        self.assertTrue(u.is_active)
+
+    @override_settings(LOGIN_FAILURE_LIMIT=None)
+    def test_login_failure_limit_not_enabled_None(self):
+        for i in range(10):
+            _ = authenticate(username=self.username, password="INCORRECT")
+        u = authenticate(username=self.username, password=self.password)
+        self.assertIsNotNone(u)
+        self.assertTrue(self.user2.is_active)
+
+    @override_settings(LOGIN_FAILURE_LIMIT=0)
+    def test_login_failure_limit_not_enabled_zero(self):
+        for i in range(10):
+            _ = authenticate(username=self.username, password="INCORRECT")
+        u = authenticate(username=self.username, password=self.password)
+        self.assertIsNotNone(u)
+        self.assertTrue(self.user2.is_active)
+
+    def test_login_failure_limit_reached(self):
+        _ = authenticate(username=self.username, password="INCORRECT")
+        _ = authenticate(username=self.username, password="INCORRECT")
+        u = authenticate(username=self.username, password=self.password)
+        self.assertIsNone(u)
+        self.assertFalse(self.user2.is_active)
+
+
+    def test_failure_counter_reset_when_reactivated(self):
+        _ = authenticate(username=self.username, password="INCORRECT")
+        _ = authenticate(username=self.username, password="INCORRECT")
+        _ = authenticate(username=self.username, password="INCORRECT")
+        # User is inactive now
+
+        # Reactivate user
+        self.user.is_active = True
+        self.user.save()
+
+        # IF the counter wasn't reset to 0, the first failed login attempt
+        # would inactivate the user again.
+        _ = authenticate(username=self.username, password="INCORRECT")
+        u = authenticate(username=self.username, password=self.password)
+        self.assertIsNotNone(u, "Should be able to log after just 1 failed login attempt")
+        self.assertTrue(self.user2.is_active)
+
