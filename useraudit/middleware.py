e9ee3977f422d9b7c18a843bc8faaf392796ba75
Dayo Ayeni <dedayoa@gmail.com>
2016-12-31 14:56:14 +0100

    Update middleware to support 1.10 style MIDDLEWARE
    Middleware has changed in django 1.10, see https://docs.djangoproject.com/en/1.10/topics/http/middleware/#upgrading-pre-django-1-10-style-middleware
    This mixin is just "band aid"

--- useraudit/middleware.py
+++ useraudit/middleware.py
@@ -1,11 +1,12 @@
 import threading
+from django.utils.deprecation import MiddlewareMixin
 
 thread_data = threading.local()
 
 def get_request():
     return getattr(thread_data, 'request', None)
 
-class RequestToThreadLocalMiddleware(object):
+class RequestToThreadLocalMiddleware(MiddlewareMixin):
 
     def process_request(self, request):
         thread_data.request = request
