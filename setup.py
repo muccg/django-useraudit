2bc51625aed25bf9e54a005133f2bc2c191f27a8
Tamas Szabo <tszabo@ccg.murdoch.edu.au>
2017-01-25 07:33:21 +0200

    Version bump 1.4.0 -> 1.5.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 setup(
     name='django-useraudit',
-    version='1.4.0',
+    version='1.5.0',
     description='Django user audit utilities like logging user log in, disabling access when password expires or user is inactive',
     long_description='Django user audit utilities like logging user log in, disabling access when password expires or user is inactive',
     author='CCG, Murdoch University',
