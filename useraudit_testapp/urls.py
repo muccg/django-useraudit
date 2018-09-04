0ad239faa9c14795beb263cf61e85958998c9f57
Tamas Szabo <tszabo@ccg.murdoch.edu.au>
2018-09-04 15:09:54 +0300

    Removing admin urls from testapp.

--- useraudit_testapp/urls.py
+++ useraudit_testapp/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls import url, include
-from django.contrib import admin
 
 
 urlpatterns = [
     url(r'^useraudit', include('useraudit.urls')),
-    url(r'^admin', admin.site.urls),
 ]
