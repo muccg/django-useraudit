f5625a784b5ffedb2cd48613a973e3fc6d6d143e
Xavi Montaña Carreras <xmontana@gmail.com>
2019-11-08 14:10:27 +0100

    Command line: 2 blank lines before class

--- useraudit/management/commands/activate_user.py
+++ useraudit/management/commands/activate_user.py
@@ -3,6 +3,7 @@ from django.contrib.auth import get_user_model
 from django.db import transaction
 from useraudit.models import UserDeactivation
 
+
 class Command(BaseCommand):
     help = """
        Activates an inactive user.
